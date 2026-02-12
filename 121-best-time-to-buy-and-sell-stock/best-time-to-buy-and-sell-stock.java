5240c81ed907b114ccf5e03ed412666c0aaabd08
Archismita Das <dasarchismita6@gmail.com>
2026-02-12 10:01:46 +0530

    Time: 1 ms (99.66%) | Memory: 94.7 MB (12.55%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices== null || prices.length<2){
+        if(prices==null||prices.length<2){
             return 0;
         }
         int min = prices[0];
         int maxP = 0;
-        for(int p:prices){
+
+        for(int p : prices){
             if(p<min){
                 min = p;
             }
             else{
-                maxP = Math.max(maxP , p-min);
+                maxP = Math.max(maxP, p-min);
             }
+
         }
         return maxP;
     }
