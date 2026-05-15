c51708047c4ba04a904a41d6c45b05bd1000f7af
Archismita Das <dasarchismita6@gmail.com>
2026-05-15 11:04:03 +0530

    Time: 1 ms (99.92%), Space: 94.6 MB (31.7%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,11 +1,8 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices==null||prices.length<2){
-            return 0;
-        }
+
         int min = prices[0];
         int maxPrice = 0;
-
         for(int p:prices){
             if(p<min){
                 min = p;
